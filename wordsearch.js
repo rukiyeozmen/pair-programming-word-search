f55821f3032edc0831a2e99aa068da06e2c0aa61
rukiyeozmen <rukiiye42@gmail.com>
2023-04-12 17:03:58 -0700

    pair programming wordearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+    if (letters.length === 0) {
+        return false;
+    }
+
+    // for each array row, find a horizontal word from the array row
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    // for each array col, find a word from the array col
+    for (let col = 0; col < letters[0].length; col++) {
+        let colStr = "";
+        for (let row = 0; row < letters.length; row++) {
+            colStr += letters[row][col];
+        }
+        if (colStr.includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+module.exports = wordSearch;
